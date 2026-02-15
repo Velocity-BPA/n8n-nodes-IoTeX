7fa1ca417a6f57bf64035eae6f31da2364e0b7d3
John Drefahl <john@velobpa.com>
2026-02-15 01:56:47 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,15 @@
-/**
- * @type {import('@types/eslint').ESLint.ConfigData}
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
  */
+
 module.exports = {
-	root: true,
-
-	env: {
-		browser: false,
-		es6: true,
-		node: true,
-	},
-
-	parser: '@typescript-eslint/parser',
-
-	parserOptions: {
-		project: ['./tsconfig.json'],
-		sourceType: 'module',
-		extraFileExtensions: ['.json'],
-	},
-
-	ignorePatterns: [
-		'.eslintrc.js',
-		'.eslintrc.prepublish.js',
-		'gulpfile.js',
-		'node_modules/**',
-		'dist/**',
-	],
-
-	plugins: ['@typescript-eslint', 'n8n-nodes-base'],
-
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:n8n-nodes-base/nodes',
-	],
-
-	rules: {
-		// TypeScript rules
-		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/no-explicit-any': 'warn',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-
-		// General rules
-		'no-console': 'warn',
-		'prefer-const': 'error',
-		'no-var': 'error',
-
-		// n8n rules - allow flexibility during development
-		'n8n-nodes-base/node-param-description-missing-final-period': 'warn',
-		'n8n-nodes-base/node-param-description-excess-final-period': 'warn',
-		'n8n-nodes-base/node-param-description-unencoded-angle-brackets': 'warn',
-		'n8n-nodes-base/node-class-description-missing-subtitle': 'off',
-		'n8n-nodes-base/node-param-options-type-unsorted-items': 'off',
-	},
-
-	overrides: [
-		{
-			files: ['*.json'],
-			plugins: ['eslint-plugin-n8n-nodes-base'],
-			extends: ['plugin:n8n-nodes-base/community'],
-			rules: {
-				'n8n-nodes-base/community-package-json-name-still-default': 'off',
-			},
-		},
-	],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
+  rules: {
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
+  },
 };

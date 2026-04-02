7d12b34e118eb4ff14a4de6b96be8bf65286241b
John Drefahl <john@velobpa.com>
2026-04-02 02:24:52 -0500

    chore: update credentials/IoTeXApi.credentials.ts to v1.0.2

--- credentials/IoTeXApi.credentials.ts
+++ credentials/IoTeXApi.credentials.ts
@@ -6,25 +6,23 @@ import {
 export class IoTeXApi implements ICredentialType {
 	name = 'ioTeXApi';
 	displayName = 'IoTeX API';
+	documentationUrl = 'https://docs.iotex.io/';
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
 			name: 'apiKey',
 			type: 'string',
-			typeOptions: {
-				password: true,
-			},
+			typeOptions: { password: true },
 			default: '',
-			required: false,
-			description: 'API key for IoTeX blockchain access. Some endpoints are public and don\'t require authentication.',
+			required: true,
+			description: 'API key for IoTeX Babel API. Obtain from the IoTeX developer portal.',
 		},
 		{
 			displayName: 'API Base URL',
 			name: 'baseUrl',
 			type: 'string',
-			default: 'https://babel-api.mainnet.iotex.io',
-			required: true,
-			description: 'Base URL for IoTeX API endpoints',
+			default: 'https://babel-api.mainnet.iotex.io/v1',
+			description: 'Base URL for the IoTeX API',
 		},
 	];
 }
